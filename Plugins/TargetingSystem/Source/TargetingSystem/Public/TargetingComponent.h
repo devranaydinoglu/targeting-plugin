88e8ee8816dc8c4cd22784ebdd33ffe31afbdcb7
devranaydinoglu <sezer146@hotmail.com>
2023-03-06 20:30:15 +0100

    Added category names and option to enable debugging

--- Plugins/TargetingSystem/Source/TargetingSystem/Public/TargetingComponent.h
+++ Plugins/TargetingSystem/Source/TargetingSystem/Public/TargetingComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2023 devran All Rights Reserved.
 
 #pragma once
 
@@ -23,89 +23,93 @@ public:
 
 protected:
 	// Radius around player character to find targets in
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	float SearchRadius;
 
 	// Time between searches
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	float SearchInterval;
 	
 	// Max horizontal angle of camera direction in which targets will be detected
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	float MaxHorizontalVisionAngle;
 
 	// Max vertical angle of camera direction in which targets will be detected
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	float MaxVerticalVisionAngle;
 
 	// Determines how much the target's angle to the camera direction affects target scores
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"), Category = "Targeting")
 	float CameraDirectionMultiplier;
 
 	// Determines how much the target's distance to the player affects target scores
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"), Category = "Targeting")
 	float DistanceMultiplier;
 
 	// Determines how much the target's angle to the player character direction affects target scores
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta = (ClampMin = "0.0", ClampMax = "1.0"), Category = "Targeting")
 	float PlayerDirectionMultiplier;
 
 	// Tag used to find targets
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	FName TargetTag;
 
 	// Trace channel used to detect targets
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	TEnumAsByte<ETraceTypeQuery> TargetTraceChannel;
 
 	// Trace channel used to detect blocking actors
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Targeting")
 	TEnumAsByte<ETraceTypeQuery> BlockingTraceChannel;
 
 	// Target reference
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, Category = "Targeting")
 	AActor* Target;
 
 	// Handle for search timer
 	FTimerHandle SearchTimerHandle;
 
 	// Player character reference
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, Category = "Targeting")
 	ACharacter* PlayerCharacter;
 
 	// Player camera reference
-	UPROPERTY(BlueprintReadOnly)
+	UPROPERTY(BlueprintReadOnly, Category = "Targeting")
 	UCameraComponent* PlayerCamera;
 
 	// Used to start timer initially
 	bool bStartTimer;
 
+	// Enable the printing of warnings and errors if any are encountered
+	UPROPERTY(EditAnywhere, Category = "Targeting")
+	bool bDebug;
+
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	// Initialize component
-	UFUNCTION(BlueprintCallable)
-	void Initialize(ACharacter* Character, UCameraComponent* Camera);
+	// Initialize component and activate targeting
+	UFUNCTION(BlueprintCallable, Category = "Targeting")
+	void Initialize(ACharacter* InPlayerCharacter, UCameraComponent* InPlayerCamera);
 
 	// Start or unpause search timer
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Targeting")
 	void ActivateTargeting();
 
 	// Pause search timer
-	UFUNCTION(BlueprintCallable)
+	UFUNCTION(BlueprintCallable, Category = "Targeting")
 	void DeactivateTargeting();
 
 	// Set optimal target as target
 	void SetTarget();
 
 	// Check if actor is in player's vision
-	bool IsInVision(AActor* Actor);
+	bool IsInVision(const AActor* Actor);
 
 	// Find most optimal target
 	AActor* FindOptimalTarget(TArray<AActor*> TargetsArray);
 
 	// Score target on each criteria
-	float ScoreTarget(AActor* Target);
+	float ScoreTarget(const AActor* Target);
 
 };
