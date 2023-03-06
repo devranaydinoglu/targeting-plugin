12ff76eec778b7b106099d30defcf54cb9bee254
devranaydinoglu <sezer146@hotmail.com>
2023-03-06 20:31:40 +0100

    Restructured code to account for debugging option and added checks for player and camera validity

--- Plugins/TargetingSystem/Source/TargetingSystem/Private/TargetingComponent.cpp
+++ Plugins/TargetingSystem/Source/TargetingSystem/Private/TargetingComponent.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 2023 devran All Rights Reserved.
 
 #include "TargetingComponent.h"
 #include "Kismet/KismetSystemLibrary.h"
@@ -8,12 +8,14 @@
 
 // Sets default values for this component's properties
 UTargetingComponent::UTargetingComponent()
+	: SearchRadius(0.0f), SearchInterval(0.0f), MaxHorizontalVisionAngle(0.0f), MaxVerticalVisionAngle(0.0f), 
+	CameraDirectionMultiplier(0.0f), DistanceMultiplier(0.0f), PlayerDirectionMultiplier(0.0f), TargetTag(""), 
+	TargetTraceChannel(ETraceTypeQuery::TraceTypeQuery1), BlockingTraceChannel(ETraceTypeQuery::TraceTypeQuery1), 
+	Target(nullptr), PlayerCharacter(nullptr), PlayerCamera(nullptr), bStartTimer(true), bDebug(false)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-
-	bStartTimer = true;
 }
 
 // Called when the game starts
@@ -28,13 +30,27 @@ void UTargetingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
-
-// Initialize component
-void UTargetingComponent::Initialize(ACharacter* Character, UCameraComponent* Camera)
+// Initialize component and activate targeting
+void UTargetingComponent::Initialize(ACharacter* InPlayerCharacter, UCameraComponent* InPlayerCamera)
 {
+	if (bDebug)
+	{
+		if (!IsValid(InPlayerCharacter))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Player Character has not been set on Initialize!"));
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Character has not been set on Initialize!"));
+		}
+		
+		if (!IsValid(InPlayerCamera))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Player Camera has not been set on Initialize!"));
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Camera has not been set on Initialize!"));
+		}
+	}
+
 	// Set references
-	PlayerCharacter = Character;
-	PlayerCamera = Camera;
+	PlayerCharacter = InPlayerCharacter;
+	PlayerCamera = InPlayerCamera;
 
 	// Start the search timer
 	ActivateTargeting();
@@ -43,15 +59,26 @@ void UTargetingComponent::Initialize(ACharacter* Character, UCameraComponent* Ca
 // Start or unpause search timer
 void UTargetingComponent::ActivateTargeting()
 {
-	// If the timer is not active yet, start it. Else unpause the timer
-	if (!bStartTimer)
+	if (IsValid(PlayerCharacter))
 	{
-		GetWorld()->GetTimerManager().UnPauseTimer(SearchTimerHandle);
+		// If the timer is not active yet, start it. Else unpause the timer
+		if (!bStartTimer)
+		{
+			GetWorld()->GetTimerManager().UnPauseTimer(SearchTimerHandle);
+		}
+		else
+		{
+			GetWorld()->GetTimerManager().SetTimer(SearchTimerHandle, this, &UTargetingComponent::SetTarget, SearchInterval, true);
+			bStartTimer = false;
+		}
 	}
 	else
 	{
-		GetWorld()->GetTimerManager().SetTimer(SearchTimerHandle, this, &UTargetingComponent::SetTarget, SearchInterval, true);
-		bStartTimer = false;
+		if (bDebug)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Player Character is not valid!"));
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Character is not valid!"));
+		}
 	}
 }
 
@@ -75,7 +102,8 @@ void UTargetingComponent::SetTarget()
 	for (const FHitResult& HitItr : Hits)
 	{
 		// Check if actor is in player's vision
-		bool bInVision = IsInVision(HitItr.GetActor());
+		bool bInVision = true;
+		if (IsValid(PlayerCamera)) bInVision = IsInVision(HitItr.GetActor());
 
 		// Check if nothing is blocking vision to target
 		FVector Start = PlayerCharacter->GetActorLocation();
@@ -104,18 +132,31 @@ void UTargetingComponent::SetTarget()
 }
 
 // Check if actor is in player's vision
-bool UTargetingComponent::IsInVision(AActor* Actor)
+bool UTargetingComponent::IsInVision(const AActor* Actor)
 {
-	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(PlayerCamera->GetComponentLocation(), Actor->GetActorLocation());
-	FRotator ActorDelta = UKismetMathLibrary::NormalizedDeltaRotator(LookAtRotation, PlayerCharacter->GetControlRotation());
-
-	// Check if target is within the vertical and horizontal vision
-	if (UKismetMathLibrary::Abs(ActorDelta.Pitch) <= MaxVerticalVisionAngle && UKismetMathLibrary::Abs(ActorDelta.Yaw) <= MaxHorizontalVisionAngle)
+	if (IsValid(PlayerCamera))
 	{
+		FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(PlayerCamera->GetComponentLocation(), Actor->GetActorLocation());
+		FRotator ActorDelta = UKismetMathLibrary::NormalizedDeltaRotator(LookAtRotation, PlayerCharacter->GetControlRotation());
+
+		// Check if target is within the vertical and horizontal vision
+		if (UKismetMathLibrary::Abs(ActorDelta.Pitch) <= MaxVerticalVisionAngle && UKismetMathLibrary::Abs(ActorDelta.Yaw) <= MaxHorizontalVisionAngle)
+		{
+			return true;
+		}
+
+		return false;
+	}
+	else
+	{
+		if (bDebug)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Player Camera is not valid!"));
+			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Camera is not valid!"));
+		}
+
 		return true;
 	}
-
-	return false;
 }
 
 // Find most optimal target
@@ -126,7 +167,7 @@ AActor* UTargetingComponent::FindOptimalTarget(TArray<AActor*> TargetsArray)
 		TArray<float> TargetScores;
 		TargetScores.Empty();
 
-		for (AActor* TempTarget : TargetsArray)
+		for (const AActor* TempTarget : TargetsArray)
 		{
 			TargetScores.Add(ScoreTarget(TempTarget));
 		}
@@ -145,26 +186,56 @@ AActor* UTargetingComponent::FindOptimalTarget(TArray<AActor*> TargetsArray)
 }
 
 // Score target on each criteria
-float UTargetingComponent::ScoreTarget(AActor* TargetToScore)
+float UTargetingComponent::ScoreTarget(const AActor* TargetToScore)
 {
 	if (IsValid(TargetToScore))
 	{
 		// Angle to camera direction
-		FVector CameraTargetUnitDirection = UKismetMathLibrary::GetDirectionUnitVector(PlayerCamera->GetComponentLocation(), TargetToScore->GetActorLocation());
-		float CameraTargetDotProduct = UKismetMathLibrary::Dot_VectorVector(PlayerCamera->GetForwardVector(), CameraTargetUnitDirection);
+		float CameraDirectionScore = 0.0f;
 
-		float CameraDirectionScore = UKismetMathLibrary::MapRangeClamped(CameraTargetDotProduct, 0.0, 1.0, 1.0, 10.0) * CameraDirectionMultiplier;
+		if (IsValid(PlayerCamera))
+		{
+			FVector CameraTargetUnitDirection = UKismetMathLibrary::GetDirectionUnitVector(PlayerCamera->GetComponentLocation(), TargetToScore->GetActorLocation());
+			float CameraTargetDotProduct = UKismetMathLibrary::Dot_VectorVector(PlayerCamera->GetForwardVector(), CameraTargetUnitDirection);
+
+			CameraDirectionScore = UKismetMathLibrary::MapRangeClamped(CameraTargetDotProduct, 0.0, 1.0, 1.0, 10.0) * CameraDirectionMultiplier;
+		}
+		else
+		{
+			if (bDebug)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Player Camera is not valid!"));
+				GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Camera is not valid!"));
+			}
+		}
 
 		// Distance to player character
-		float Distance = PlayerCharacter->GetDistanceTo(TargetToScore);
-
-		float DistanceScore = UKismetMathLibrary::MapRangeClamped(Distance, 0.0, SearchRadius, 10.0, 1.0) * DistanceMultiplier;
+		float DistanceScore = 0.0f;
 
 		// Angle to player character direction
-		FVector PlayerTargetUnitDirection = UKismetMathLibrary::GetDirectionUnitVector(PlayerCharacter->GetActorLocation(), TargetToScore->GetActorLocation());
-		float PlayerTargetDotProduct = UKismetMathLibrary::Dot_VectorVector(PlayerCharacter->GetActorForwardVector(), PlayerTargetUnitDirection);
+		float PlayerDirectionScore = 0.0f;
 
-		float PlayerDirectionScore = UKismetMathLibrary::MapRangeClamped(PlayerTargetDotProduct, 0.0, 1.0, 1.0, 10.0) * PlayerDirectionMultiplier;
+		if (IsValid(PlayerCharacter))
+		{
+			// Distance to player character
+			float Distance = PlayerCharacter->GetDistanceTo(TargetToScore);
+
+			DistanceScore = UKismetMathLibrary::MapRangeClamped(Distance, 0.0, SearchRadius, 10.0, 1.0) * DistanceMultiplier;
+
+			// Angle to player character direction
+			FVector PlayerTargetUnitDirection = UKismetMathLibrary::GetDirectionUnitVector(PlayerCharacter->GetActorLocation(), TargetToScore->GetActorLocation());
+			float PlayerTargetDotProduct = UKismetMathLibrary::Dot_VectorVector(PlayerCharacter->GetActorForwardVector(), PlayerTargetUnitDirection);
+
+			PlayerDirectionScore = UKismetMathLibrary::MapRangeClamped(PlayerTargetDotProduct, 0.0, 1.0, 1.0, 10.0) * PlayerDirectionMultiplier;
+		}
+		else
+		{
+			if (bDebug)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Player Character is not valid!"));
+				GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Orange, TEXT("Player Character is not valid!"));
+			}
+		}
 
 		// Calculate final score
 		float FinalScore = CameraDirectionScore + DistanceScore + PlayerDirectionScore;
